d4205490c38bfdce9dd2dece6a681143db8e2df6
Miika Hänninen <miika.hanninen@reaktor.com>
2017-04-28 11:48:52 +0300

    Add useful tests

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -2,7 +2,19 @@
 
 . ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
 
-testCompileRuns() {
+testCompileWithoutNpmTokenIsNoOp() {
     compile
+
     assertCapturedSuccess
+    if [ -f "$HOME/.npmrc" ]; then
+        fail "npmrc was written even though no NPM_TOKEN was present"
+    fi
+}
+
+testCompileWithNpmTokenCreatesProperNpmrc() {
+    echo "00000000-0000-0000-0000-000000000000" > "$ENV_DIR/NPM_TOKEN"
+
+    compile
+    
+    assertFileMD5 "7738ff0e43d2ed3822b443cee90f6372" "$HOME/.npmrc"
 }
